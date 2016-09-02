f0d959d44887126f579fb27e28dfb7f9f53cabf8
Nathanael Lamelliere <nla@zenexity.com>
2016-09-02 09:58:50 +0200

    update video size

--- app/views/slices/express.php
+++ app/views/slices/express.php
@@ -1,5 +1,5 @@
 <div class="youtube">
 
-<iframe width="1280" height="720" src="https://www.youtube.com/embed/L7FjR3b3Jyc?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/L7FjR3b3Jyc?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
 
 </div>
